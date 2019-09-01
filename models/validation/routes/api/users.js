e9947456cabc78286621114fb008c9bf544d183e
Janessa Tran <tranjanessa@gmail.com>
2019-08-31 20:19:40 -0500

    Setup route for user registartion/login

--- models/validation/routes/api/users.js
+++ models/validation/routes/api/users.js
@@ -10,3 +10,40 @@ const validateLoginInput = require('../../validation/login');
 
 // load user model
 const User = require('../../models/User');
+
+// @route POST api/users/register
+// @desc register user
+// @access public
+router.post('/register', (req, res) => {
+  // form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      // hash password before saving in DB
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
