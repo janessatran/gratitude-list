479f555dcacecbcb3cb93336edc01713bbe9702c
Janessa Tran <tranjanessa@gmail.com>
2019-09-02 13:46:12 -0500

    Updates

---
+++ precache-manifest.280dae877ad9dad0f805c7cf08d72694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2676e2be03b19af8ab4d523d505d6dc",
+    "url": "/gratitude-list/index.html"
+  },
+  {
+    "revision": "e0d88d22536a027d77c6",
+    "url": "/gratitude-list/static/css/main.0ee1cd3b.chunk.css"
+  },
+  {
+    "revision": "ff5e1a48ac7fbdbc335e",
+    "url": "/gratitude-list/static/js/2.4f541128.chunk.js"
+  },
+  {
+    "revision": "e0d88d22536a027d77c6",
+    "url": "/gratitude-list/static/js/main.39bae82d.chunk.js"
+  },
+  {
+    "revision": "b0051cdbb70cacf04822",
+    "url": "/gratitude-list/static/js/runtime~main.9ccdd0e0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gratitude-list/precache-manifest.280dae877ad9dad0f805c7cf08d72694.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gratitude-list/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
